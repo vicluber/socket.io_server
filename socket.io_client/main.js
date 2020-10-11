fb5146fbcbcbe37029b55e040a423ffb49274a88
Victor Willhuber <victorwillhuber@gmail.com>
2020-10-11 19:54:20 -0300

    Saving on git the client side

---
+++ socket.io_client/main.js
@@ -0,0 +1,27 @@
+var socket = io.connect('http://3.12.85.150:8080/', { 'forceNew': true }); // Esta ip y puerto es el determinado para mi web socket server en mi amazon web serive.
+
+socket.on('messages', function(data) {
+  console.log(data);
+  render(data);
+})
+
+function render (data) {
+  var html = data.map(function(elem, index) {
+    return(`<div>
+              <strong>${elem.author}</strong>:
+              <em>${elem.text}</em>
+            </div>`);
+  }).join(" ");
+
+  document.getElementById('messages').innerHTML = html;
+}
+
+function addMessage(e) {
+  var message = {
+    author: document.getElementById('username').value,
+    text: document.getElementById('texto').value
+  };
+
+  socket.emit('new-message', message);
+  return false;
+}
